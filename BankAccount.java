4bf07331e439955f5b1dc654a8c4cfaf3beee9ba
TM0D <37343661+TM0D@users.noreply.github.com>
2021-01-13 13:52:58 -0600

    init commit, has been stored locally for a while

---
+++ BankAccount.java
@@ -0,0 +1,78 @@
+public class BankAccount {
+    private double balance;
+    private int accountID;
+    private String userName;
+
+    // Java Setup Classes
+
+    public BankAccount() {
+        balance = 0;
+        accountID = 0; // make random id
+        userName = "";
+    }
+
+    public BankAccount(int accountIDIn, String user) {
+        balance = 0;
+        accountID = accountIDIn;
+        userName = user;
+    }
+
+    public String toString() {
+        if (userName.equals("")) {
+            return accountID + ": $" + balance;
+        } else {
+            return userName + ": $" + balance;
+        }
+    }
+
+    // Accessors
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public double getBal() {
+        return balance;
+    }
+
+    public String getUsername() {
+        return userName;
+    }
+    
+    // Check Methods
+
+    private boolean checkID() {
+        if (accountID == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Vairables
+
+    public void setBal(double newBal) {
+        if (checkID()) {
+            System.out.print("Requires Accound ID. Balance is $" + balance);
+            return;
+        }
+        balance = newBal;
+    }
+
+    public void deposit(double addToBal){
+        if (checkID()) {
+            System.out.print("Requires Accound ID. Balance is $" + balance);
+            return;
+        } 
+        balance += addToBal;
+    }
+
+    public void withdrawl(double rmFromBal){
+        if (checkID()) {
+            System.out.print("Requires Accound ID. Balance is $" + balance);
+            return;
+        } 
+        balance -= rmFromBal;
+    }
+
+}
---
+++ oldAPCSACode.java
@@ -0,0 +1,139 @@
+import java.util.Scanner;
+
+/**
+ * This is just a bunch of olf methods i quickly wrote for little things
+ * throughout about 4 units, usually by sorted by date written. Few have
+ * Javadoc already, but I might change that, idk.
+ * 
+ */
+public class oldAPCSACode {
+  // Unit 1 & 2
+  public static String getInitials(String fullName) {
+    // BAD PRACTICE DON'T DO THIS
+    fullName = fullName.trim();
+    fullName = " " + fullName;
+    int secondSpace = fullName.indexOf(" ", 2) + 1;
+    int thirdSpace = fullName.indexOf(" ", secondSpace + 1) + 1;
+
+    String It = fullName.substring(1, 2) + fullName.substring(secondSpace, secondSpace + 1)
+        + fullName.substring(thirdSpace, thirdSpace + 1);
+    // System.out.println(secondSpace);
+
+    return It.toUpperCase();
+  }
+
+  public static int doubleToInt(double a) {
+    return (int) a;
+  }
+
+  public static void mathMethods() {
+    int test;
+    for (int i = 0; i < 100; i++) {
+      test = (int) (Math.random() * 5) + 3;
+      System.out.print(test);
+    }
+  }
+
+  /**
+   * Given a string, if the string begins with "red" or "blue" return that color
+   * string, otherwise return the empty string. seeColor("redxx") --> "red"
+   * seeColor("xxred") --> "" seeColor("blueTimes") --> "blue"
+   */
+  public static String seeColor(String str) {
+    String redSub = "";
+    String blueSub = "";
+    if (str.length() > 2) {
+      redSub = str.substring(0, 3);
+    }
+    if (str.length() > 3) {
+      blueSub = str.substring(0, 4);
+    }
+    if (redSub.equals("red")) {
+      return "red";
+    } else if (blueSub.equals("blue")) {
+      return "blue";
+    } else {
+      return "";
+    }
+  }
+  public static String seeColorV2(String str) {
+    str = str + "xxxxxx";
+    String redSub = str.toLowerCase().substring(0, 3);
+    String blueSub = str.toLowerCase().substring(0, 4);
+    if (redSub.equals("red")) {
+      return "red";
+    } else if (blueSub.equals("blue")) {
+      return "blue";
+    } else {
+      return "";
+    }
+  }
+
+  public static boolean lessBy10(int a, int b, int c){
+   int ap = a - 10; int bp = b - 10; int cp = c - 10;
+   if (a <= bp || a <= cp || b <= ap || b <= cp || c <= ap || c <= bp){
+    return true;
+   } else {
+     return false;
+   }
+  }
+
+  public static void guessGame() {
+    int throwAway = (int)(Math.random() * 100);
+    System.out.println("I have chosen a number between 1 and 100. Guess: " + throwAway);
+    Scanner input = new Scanner(System.in);
+    String guess = input.nextLine();
+    while (!(Integer.valueOf(guess) == throwAway)) {
+      int intGuess = Integer.valueOf(guess);
+      if (intGuess > throwAway) {
+        System.out.print("Too High!");
+      } else {
+        System.out.print("Too Low!");
+      }
+      System.out.println(" Try again!");
+      guess = input.nextLine();
+    }
+    System.out.println("You got it!");
+    input.close();
+  }
+
+/**
+   * Prints the first num self-devisors that 
+   * are greator than
+   * or equal to start
+   * 
+   * @Note APCSA.FRQ
+   * @see isSelfDevisor
+   */
+  public static void firstNumSelfDivisors(int start, int total) {
+    for(int i = 0; i < total; i++) {
+      while(!isSelfDevisor(start)){
+        start++;
+      }
+      System.out.println(start);
+      start++;
+    }
+  }
+
+  /**
+   * Returns true/false depending weather 
+   * or not number is a self devisor.
+   * 
+   * @note APCSA.FRQ
+   * @see firstNumSelfDivisors
+   */
+  public static boolean isSelfDevisor(int number) {
+    int n = number;
+    while (!(n == 0)) {
+      int d = n % 10;
+      if (d == 0) {
+        return false;
+      } else if (number % d != 0) {
+        return false;
+      } else {
+       n = n / 10;
+      }
+    }
+    return true;
+  }
+}
